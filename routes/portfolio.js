23377255c18b05cbab47510e6dca586ce0bdd155
Jaewon Kim <jaewonism57@gmail.com>
2016-06-02 23:36:15 +0900

    add portfolios

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res, next) {
         title: 'Jaewonism',
         url: req.protocol + '://' + req.headers.host + req.baseUrl + req.url,
         image: req.protocol + '://' + req.headers.host + '/images/portfolio/goodoc.png',
-        description: '포트폴리오 리스트입니다. 굿닥, Easy-map.js, Ciceron 등의 웹 페이지 및 오픈소스 개발을 하였습니다. Ruby on Rails, Node JS, Swift 에 관심이 많습니다.'.substring(0, 255),
+        description: '포트폴리오 리스트입니다. 굿닥, Easy-map.js, Image-preview.js, Ciceron 등의 웹 페이지 및 오픈소스 개발을 하였습니다. Ruby on Rails, Node JS, Swift 에 관심이 많습니다.'.substring(0, 255),
         keyword: 'portfolio, Front End, 포트폴리오, 웹개발자, 프론트엔드, Java Script, Node JS, Ruby on Rails, swift',
         userId: req.user ? req.user.user_id : null
       }
@@ -46,8 +46,6 @@ router.get('/map', function (req, res, next) {
 });
 
 router.get('/ciceron', function (req, res, next) {
-  res.redirect('/portfolio');
-  /*
   res.render('portfolio/ciceron',
       {
         req: req,
@@ -59,7 +57,20 @@ router.get('/ciceron', function (req, res, next) {
         userId: req.user ? req.user.user_id : null
       }
   );
-  */
+});
+
+router.get('/preview', function (req, res, next) {
+  res.render('portfolio/preview',
+      {
+        req: req,
+        title: 'Jaewonism',
+        url: req.protocol + '://' + req.headers.host + req.baseUrl + req.url,
+        image: req.protocol + '://' + req.headers.host + '/images/portfolio/preview.png',
+        description: '이미지 업로드 시 유효성 검사, 미리보기 기능 등을 제공하는 Java Script Open Source 입니다.'.substring(0, 255),
+        keyword: 'portfolio, Front End, Image Upload, Open Source, 포트폴리오, 웹개발자, Java Script, 오픈소스',
+        userId: req.user ? req.user.user_id : null
+      }
+  );
 });
 
 module.exports = router;
