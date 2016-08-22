e12fbd622abf9661148a9ac0308fe34ba55e93d4
Jaewon Kim <jaewonism57@gmail.com>
2016-08-23 00:20:36 +0900

    add monitor

--- build/main.js
+++ build/main.js
@@ -182,6 +182,28 @@ var options = {
 
 var app = (0, _express2.default)();
 
+/*
+app.use(require('express-status-monitor')({
+  title: 'Express Status',  // Default title
+  path: '/status',
+  auth: {
+    // No authentication at all
+    username: config.name,
+    password: config.password
+  },
+  spans: [{
+    interval: 1,            // Every second
+    retention: 60           // Keep 60 datapoints in memory
+  }, {
+    interval: 5,            // Every 5 seconds
+    retention: 60
+  }, {
+    interval: 15,           // Every 15 seconds
+    retention: 60
+  }]
+}));
+*/
+
 // view engine setup
 app.set('views', _path2.default.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
--- server/main.js
+++ server/main.js
@@ -72,6 +72,28 @@ const options = {
 
 const app = express();
 
+/*
+app.use(require('express-status-monitor')({
+  title: 'Express Status',  // Default title
+  path: '/status',
+  auth: {
+    // No authentication at all
+    username: config.name,
+    password: config.password
+  },
+  spans: [{
+    interval: 1,            // Every second
+    retention: 60           // Keep 60 datapoints in memory
+  }, {
+    interval: 5,            // Every 5 seconds
+    retention: 60
+  }, {
+    interval: 15,           // Every 15 seconds
+    retention: 60
+  }]
+}));
+*/
+
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
