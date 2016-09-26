afae8e61b902ceb050f052ce4618cdd4bd6bb888
wonism <jaewonism57@gmail.com>
2016-09-26 20:28:29 +0900

    modify test code

--- src/js/test.js
+++ src/js/test.js
@@ -1,26 +1,30 @@
 if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+  alert('NOT SUPPORT');
   // return;
-}
+} else {
+  navigator.mediaDevices.enumerateDevices()
+    .then(function (devices) {
+      alert('GET DEVICE');
+      var str = '';
 
-navigator.mediaDevices.enumerateDevices()
-  .then(function (devices) {
-    var str = '';
+      devices.forEach(function (device) {
+        str += device.kind + ': ' + device.label + ' id = ' + device.deviceId + '<br />';
+      });
 
-    devices.forEach(function (device) {
-      str += device.kind + ': ' + device.label + ' id = ' + device.deviceId + '<br />';
+      alert(str);
+      document.write(str);
+    })
+    .catch(function (err) {
+      alert('ERROR OCCURED');
+      console.log(err.name + ': ' + error.message);
     });
 
-    document.write(str);
-  })
-  .catch(function (err) {
-    console.log(err.name + ': ' + error.message);
-  });
+  var constraints = {
+    video: {
+      // optional: [ { sourceId: 'a6bcaea7a6bce98e06a1917e4380cdfa7c04e5245ae3e8ef7f78591adefc06d0' } ]
+    }
+  };
 
-var constraints = {
-  video: {
-    // optional: [ { sourceId: 'a6bcaea7a6bce98e06a1917e4380cdfa7c04e5245ae3e8ef7f78591adefc06d0' } ]
-  }
-};
-
-navigator.getUserMedia(constraints, function () { alert('성공'); }, function () { alert('실패'); } );
+  navigator.getUserMedia(constraints, function () { alert('SUCCESS'); }, function () { alert('FAIL'); } );
+}
 
