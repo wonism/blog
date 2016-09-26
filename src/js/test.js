9d7c2f80bf55893b3b604a1d919d757c51adc621
wonism <jaewonism57@gmail.com>
2016-09-26 20:34:26 +0900

    get rear camera

--- src/js/test.js
+++ src/js/test.js
@@ -1,30 +1,42 @@
-if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
-  alert('NOT SUPPORT');
-  // return;
-} else {
-  navigator.mediaDevices.enumerateDevices()
-    .then(function (devices) {
-      alert('GET DEVICE');
-      var str = '';
+navigator.getUserMedia(
+  {
+    video: {
+      facingMode: { exact: "environment" }
+    }
+  },
+  function () {
+    alert('SUCCESS');
+  },
+  function () {
+    alert('FAIL');
+  });
 
-      devices.forEach(function (device) {
-        str += device.kind + ': ' + device.label + ' id = ' + device.deviceId + '<br />';
+
+if (false) {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+    // NOT SUPPORT
+    // return;
+  } else {
+    navigator.mediaDevices.enumerateDevices()
+      .then(function (devices) {
+        var str = '';
+
+        devices.forEach(function (device) {
+          str += device.kind + ': ' + device.label + ' id = ' + device.deviceId + '<br />';
+        });
+
+        console.log(str);
+      })
+      .catch(function (err) {
+        console.log(err.name + ': ' + error.message);
       });
 
-      alert(str);
-      document.write(str);
-    })
-    .catch(function (err) {
-      alert('ERROR OCCURED');
-      console.log(err.name + ': ' + error.message);
-    });
+    var constraints = {
+      video: {
+        // optional: [ { sourceId: 'a6bcaea7a6bce98e06a1917e4380cdfa7c04e5245ae3e8ef7f78591adefc06d0' } ]
+      }
+    };
 
-  var constraints = {
-    video: {
-      // optional: [ { sourceId: 'a6bcaea7a6bce98e06a1917e4380cdfa7c04e5245ae3e8ef7f78591adefc06d0' } ]
-    }
-  };
-
-  navigator.getUserMedia(constraints, function () { alert('SUCCESS'); }, function () { alert('FAIL'); } );
+    navigator.getUserMedia(constraints, function () { alert('SUCCESS'); }, function () { alert('FAIL'); } );
+  }
 }
-
